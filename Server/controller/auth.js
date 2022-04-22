c9779e9dc2a637cb4a6d28fab31d02fb7fba373d
Mr Gholam <mobingh093@gmail.com>
2022-04-22 16:54:11 +0430

    validation added for signup and login

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -1,41 +1,46 @@
 // importing bcrypt 
 const bcrypt = require('bcryptjs')
+// importing validation result
+const { validationResult } = require('express-validator')
 //importing user model
 const User = require('../model/user')
 // Post Sign up controller
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
+    // Looking for validation errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const email = req.body.email
     const username = req.body.username
     const password = req.body.password
-    // Looking for similar email
-    User.findOne({ where: { email: email } }).then(result => {
-        if (!result) {
+    try {
+        // Looking for similar email
+        const emailFound = await User.findOne({ where: { email: email } })
+        if (!emailFound) {
             // Looking for similar username
-            User.findOne({ where: { username } }).then(result => {
-                if (!result) {
-                    // hashing the password
-                    bcrypt.hash(password, 12).then(hashedPassword => {
-                        // creating the new user
-                        User.create({
-                            email: email,
-                            username: username,
-                            password: hashedPassword
-                        }).then(result => {
-                            // console.log(result)
-                            res.status(301).json({
+            const usernameFound = await User.findOne({ where: { username } })
+            if (!usernameFound) {
+                // hashing the password
+                const hashedPassword = await bcrypt.hash(password, 12)
+                // creating the new user
+                const newUser = await User.create({
+                    email: email,
+                    username: username,
+                    password: hashedPassword
+                })
 
-                            })
-                        })
-                    })
+                res.status(301).json({
 
-                }
-                // found similar username in database
-                else {
-                    res.status(403).json({
-                        UsernameErr: 'This Username has been used before'
-                    })
-                }
-            })
+                })
+
+            }
+            // found similar username in database
+            else {
+                res.status(403).json({
+                    UsernameErr: 'This Username has been used before'
+                })
+            }
 
         }
         // found similar Email in data base
@@ -44,42 +49,45 @@ exports.postSignup = (req, res, next) => {
                 emailErr: 'This Email has been used before'
             })
         }
-    }).catch(err => {
+    } catch (err) {
         console.log(err)
-    })
+    }
+
+
 }
 //  Post Login controller
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     const email = req.body.email
     const password = req.body.password
     // Looking for email in data base
-    User.findOne({ where: { email } }).then(result => {
-        // email found 
-        if (result) {
-            // comparing the password 
-            bcrypt.compare(password, result.password).then(doMatch => {
-                // passwords match
-                if (doMatch) {
-                    res.status(200).json({
-                        IsLoggedIn: true
-                    })
-                }
-                // passwords do not match
-                else {
-                    res.status(403).json({
-                        IsLoggedIn: false
-                    })
-                }
-
-            }
-            )
-        }
-        // email not found
-        else {
-            res.status(403).json({
-                emailErr: 'Email not found'
+    const emailFound = await User.findOne({ where: { email } })
+    // email found 
+    if (emailFound) {
+        // comparing the password 
+        const matchPassword = await bcrypt.compare(password, result.password)
+        // passwords match
+        if (matchPassword) {
+            res.status(200).json({
+                IsLoggedIn: true
             })
         }
+        // passwords do not match
+        else {
+            res.status(403).json({
+                IsLoggedIn: false
+            })
+        }
+
     }
-    )
+
+    // email not found
+    else {
+        res.status(403).json({
+            emailErr: 'Email not found'
+        })
+    }
 }
--- Server/router/auth.js
+++ Server/router/auth.js
@@ -1,14 +1,30 @@
 const express = require('express')
+// importing express-validator
+const { body } = require('express-validator')
+
 
 const authController = require('../controller/auth')
 
 const router = express.Router()
 
 // post signup
-router.post('/signup', authController.postSignup)
+router.post('/signup', [
+    body('email').isEmail().normalizeEmail().notEmpty(),
+    body('username').trim().isLength({ min: 3 }).notEmpty(),
+    body('password').trim().isLength({ min: 8 }).isAlphanumeric().notEmpty(),
+    body('confirmPassword').trim().custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password');
+        }
+        return true
+    })
+], authController.postSignup)
 
 // post login
-router.post('/login', authController.postLogin)
+router.post('/login', [
+    body('email').isEmail().normalizeEmail().notEmpty(),
+    body('password').trim().isAlphanumeric().isLength({ min: 8 })
+], authController.postLogin)
 
 
 module.exports = router
